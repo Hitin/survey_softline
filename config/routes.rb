06140a964622bfd000908294ab22ea03e55f3f70
Petr Ablyazov <phoenix-h@mail.ru>
2020-01-27 13:16:41 +0400

    Show admin.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resource :surveys
     resource :session, only: [:new, :create, :destroy]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :users
+  end
+
 end
