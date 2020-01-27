4d81f24b119422d970222c201714541ccc4d0500
Petr Ablyazov <phoenix-h@mail.ru>
2020-01-27 17:04:16 +0400

    Serializer Survey. Survey index show create

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -1,3 +1,42 @@
 class Api::V1::SurveysController < Api::V1::ApplicationController
+  def index
+    q_params = params[:q] || { s: 'id asc' }
+
+    surveys = Survey.all
+      .ransack(q_params)
+      .result
+      .page(params[:page])
+      .per(params[:per_page])
+      .includes(:author)
+
+
+    json = {
+        items: surveys.map { |s| SurveySerializer.new(s).as_json },
+        meta: build_meta_surveys(surveys)
+    }
+
+    respond_with json
+  end
+
+  def show
+    survey = Survey.find(params[:id])
+    respond_with(survey)
+  end
+
+  def create
+    survey = current_user.my_surveys.new(survey_params)
+
+    if survey.save
+      respond_with(survey, location: nil)
+    else
+      render(json: { errors: survey.errors.messages }, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, author_id: current_user.id)
+  end
 
 end
--- test/controllers/api/v1/surveys_controller_test.rb
+++ test/controllers/api/v1/surveys_controller_test.rb
@@ -1,5 +1,33 @@
 require 'test_helper'
 
-class Api::V1::TasksControllerTest < ActionController::TestCase
+class Api::V1::SurveysControllerTest < ActionController::TestCase
+  test "should get show" do
+    author = create :user
+    survey = create :survey, author: author
+    get :show, params: { id: survey.id, format: :json }
+    assert_response :success
+  end
+
+  test "should get index" do
+    get :index, params: { format: :json }
+    assert_response :success
+  end
+
+  test "should post create" do
+    author = create :user
+    sign_in(author)
+    survey_attributes = attributes_for(:survey)
+    post :create, params: { survey: survey_attributes, format: :json }
+    assert_response :created
+
+    data = JSON.parse(response.body)
+    created_survey = Survey.find(data['id'])
+
+    survey_attributes = survey_attributes.merge({ author_id: author.id })
+    
+
+    assert created_survey.present?
+    assert_equal survey_attributes.stringify_keys, created_survey.slice(*survey_attributes.keys)
+  end
 
 end
