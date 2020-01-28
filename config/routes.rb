7f7650d3afed6b55ec302bf84b3711645b5ba9c5
Petr Ablyazov <phoenix-h@mail.ru>
2020-01-28 11:37:48 +0400

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :surveys, only: [:index, :show, :create, :update, :destroy]
+      resources :questions, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
