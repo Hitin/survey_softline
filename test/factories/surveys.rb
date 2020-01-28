3893950259fb4a2faffc1503ec9a92beabac063d
Petr Ablyazov <phoenix-h@mail.ru>
2020-01-28 14:32:03 +0400

    Add trait in Factories Survey

--- test/factories/surveys.rb
+++ test/factories/surveys.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :survey do
     title
     author_id { nil }
+
+    trait :with_author do
+      author { create :user }
+      author_id { author.id }
+    end
+
   end
 end
